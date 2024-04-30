8759412b73fa8457e8a10ac3750bf48cebde08e8
git-pietro <pi.orazio11@gmail.com>
2024-04-30 12:03:29 +0200

    Add Chirpy to doc folder

---
+++ Documentation/_site/assets/js/data/swconf.js
@@ -0,0 +1,34 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1714471195',resources: [
+      '/SideProjectst/Documentation/assets/css/jekyll-theme-chirpy.css',
+      '/SideProjectst/Documentation/',
+      
+        '/SideProjectst/Documentation/categories/',
+      
+        '/SideProjectst/Documentation/tags/',
+      
+        '/SideProjectst/Documentation/archives/',
+      
+        '/SideProjectst/Documentation/about/',
+      
+
+      
+      
+    ],allowHosts: [
+      
+        'chirpy-img.netlify.app',
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'cdnjs.cloudflare.com'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
